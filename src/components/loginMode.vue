6baf03f0a727d67ada2fd9b9a7609d5a81797570
LeroyK <bridieiybnf@gmail.com>
2023-01-23 06:29:38 +0800

    2023年1月23日

--- src/components/loginMode.vue
+++ src/components/loginMode.vue
@@ -11,15 +11,19 @@
     placeholder="Please input password"
     show-password
   />
-    <el-image class="codeImg" :src="imgDataURL" fit="contain" @click="changeCode"/>
+    <el-image class="codeImg" :src="imgDataURL" fit="cover" @click="changeCode"/>
     <el-input v-model="code" placeholder="Please input code"></el-input>
     </div>
-    <el-button type="primary">
-      登录<el-icon class="el-icon--right"><Upload /></el-icon>
+    <div class="btnLogin">
+      <el-button type="primary" @click="btnLogin" :loading="loading">
+      {{ labelText.btnLogin }}
     </el-button>
-    <el-link>
-      注册账号<el-icon class="el-icon--right"><icon-view /></el-icon>
+    </div>
+    <div class="linkRegister">
+      <el-link @click="linkRegister">
+      {{ labelText.linkRegister }}<el-icon><DArrowRight /></el-icon>
     </el-link>
+    </div>
   </div>
 </template>
 <script lang="ts" setup>
@@ -27,6 +31,8 @@ import { useLanguage } from "@/stores/recordLanguage"
 import { useTranslate } from "@/hooks/useTranslate"
 import { reactive, ref } from "vue"
 import { useRequest } from "@/hooks/request";
+import { DArrowRight } from "@element-plus/icons-vue"
+import {useAuthUser }from "@/stores/authUser"
 // 生成随机验证码，本来应该选择请求后端。
 import verification from 'verification-code';
 let result = verification.create()
@@ -37,11 +43,17 @@ const imgDataURL = ref(result.dataURL)
 
 const username = ref('')
 const password = ref('')
+const loading = ref(false)
+
 const code = ref("")
 const labelText = reactive({
   username: '用户名',
-  password: '密码'
+  password: '密码',
+  btnLogin: "登录",
+  linkRegister: "注册账号"
 })
+
+
 
 
 const changeCode = ()=>{
@@ -50,9 +62,26 @@ const changeCode = ()=>{
   imgDataURL.value = result.dataURL
 }
 
+const wait = (ms:number) => new Promise((resolve) => setTimeout(resolve, ms));
 
 
+const btnLogin = async () => {
+  loading.value = true
+  // 模仿数据请求
+  await wait(2000)
+  loading.value = false
+  if (username.value === "admin" && password.value === "admin" && vcode.value===code.value ) {
+    console.log("验证成功，跳转路由拦截页面(必须后端验证)，保存加密后的token");
+  } else {
+    changeCode()
+    console.log("验证失败, 弹窗提示");
+  }
+}
 
+
+const linkRegister = ()=>{
+  console.log("弹出对话框，形成遮盖");
+}
 
 
 
@@ -73,6 +102,16 @@ const changeCode = ()=>{
     margin-bottom: 1rem;
   }
 
-  
+  .model {
+    .codeImg {
+      cursor: pointer;
+    }
+  }
+
+  .btnLogin {
+    font-size: 45px;
+  }
+
+
 }
 </style>
