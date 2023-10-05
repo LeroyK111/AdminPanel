6d7f8bd6c2235911d4d95e83aec7bf2ceeeaba35
LeroyK <bridieiybnf@gmail.com>
2023-10-05 12:43:44 +0800

    1

--- src/components/help.vue
+++ src/components/help.vue
@@ -1,11 +1,40 @@
 <template>
   <div>
     <h1>帮助</h1>
+    <!-- 监听子组件的生命周期 -->
+    <!-- <List @hook:mounted="listenMounted" /> -->
   </div>
 </template>
 <script lang="ts" setup>
 
+
+
+
+
 </script>
-<style lang="less">
-  
+<style lang="less" scoped>
+
+// 两种组件样式穿透方法
+// /deep/ .el-checkbox {
+//   display: block;
+//   font-size: 26px;
+
+//   .el-checkbox__label {
+//     font-size: 16px;
+//   }
+// }
+
+// Outer layer >>> .el-checkbox {
+//   display: block;
+//   font-size: 26px;
+
+//   .el-checkbox__label {
+//     font-size: 16px;
+//   }
+// }
+
+
+
+
+
 </style>
